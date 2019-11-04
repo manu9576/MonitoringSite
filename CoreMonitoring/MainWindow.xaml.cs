848725188d9001662ca50a326aa43413060e06a9
manu <manu9576@hotmail.fr>
2019-11-04 22:28:19 +0100

    Creation of Core project

---
+++ CoreMonitoring/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using MonitoringSite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoreMonitoring
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Worker m_Worker = null;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            m_Worker = new Worker();
+            m_Worker.LoadParamaters();
+            DataContext = m_Worker.Parameter;
+
+        }
+
+        ~MainWindow()
+        {
+            if (m_Worker != null)
+            {
+                m_Worker.SaveParameter();
+                m_Worker.Stop();
+            }
+        }
+
+        private void Bt_action_Click(object sender, RoutedEventArgs e)
+        {
+            m_Worker.AddSite(tb_NomSite.Text);
+        }
+
+        private void Bt_delete_Click(object sender, RoutedEventArgs e)
+        {
+            object item = dg_Sites.SelectedItem;
+
+            if (item != null && item is SiteParameters site)
+            {
+                m_Worker.RemoveSite(site.SiteName);
+            }
+        }
+
+        private void Parameter_Click(object sender, RoutedEventArgs e)
+        {
+            Parameters windowParameter = new Parameters(m_Worker.Parameter);
+
+            windowParameter.ShowDialog();
+
+            windowParameter.Close();
+        }
+
+    }
+}
---
+++ CoreMonitoring/Parameters.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MonitoringSite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoreMonitoring
+{
+    /// <summary>
+    /// Logique d'interaction pour Parameters.xaml
+    /// </summary>
+    public partial class Parameters : Window
+    {
+        public Parameters(GlobalParameters globalParameter)
+        {
+                InitializeComponent();
+
+                DataContext = globalParameter;
+        }
+    }
+}
